70b4e16499d6820711f885abeda0367f6cdf7acb
Alexandr Borisov <sasha@ubuntuSasha>
2015-08-25 18:20:18 +0300

    додано репорти та переключення між ними, а також датепікер (не підключено)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('hello');
+var UIA = {
+		init : function(){
+			$('._repMenuClose').on('click', function(){
+				$('._repMenu').hide();
+				
+			});
+			$('._repMenuOpen').on('click', function(){
+				$('._repMenu').show();
+				
+			});
+			
+			$('._repMenu a').click(function (e) {
+				  e.preventDefault();
+				  $('._repRightTopHelp').text($(this).text());
+				  $(this).tab('show')
+			})
+		}
+}
+
+$(document).ready(function(){
+	UIA.init();
+});
